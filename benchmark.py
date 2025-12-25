b1eba11ccd633c960903cce167de18e127e85c96
Vishnu Sai Priya <138894727+vishnusaipriya7@users.noreply.github.com>
2025-12-25 19:31:30 +0530

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -8,18 +8,12 @@ import tensorflow as tf
 import numpy as np
 import time
 
-
-# ---------------------------------------------------------
 # Config
-# ---------------------------------------------------------
 TFLITE_MODEL_PATH = "tflite_models/cnn_int8.tflite"
 NUM_RUNS = 100
 WARMUP_RUNS = 10
 
-
-# ---------------------------------------------------------
 # Load TFLite Model
-# ---------------------------------------------------------
 print("📦 Loading TFLite model...")
 
 interpreter = tf.lite.Interpreter(model_path=TFLITE_MODEL_PATH)
@@ -34,10 +28,7 @@ input_dtype = input_details[0]["dtype"]
 print(f"Input shape: {input_shape}")
 print(f"Input dtype: {input_dtype}")
 
-
-# ---------------------------------------------------------
 # Create Dummy Input (UINT8)
-# ---------------------------------------------------------
 input_data = np.random.randint(
     low=0,
     high=255,
@@ -45,19 +36,16 @@ input_data = np.random.randint(
     dtype=np.uint8
 )
 
-
-# ---------------------------------------------------------
 # Warm-up Runs (important for fair timing)
-# ---------------------------------------------------------
+
 print("🔥 Warming up...")
 for _ in range(WARMUP_RUNS):
     interpreter.set_tensor(input_details[0]["index"], input_data)
     interpreter.invoke()
 
 
-# ---------------------------------------------------------
 # Benchmark
-# ---------------------------------------------------------
+
 print("⏱️ Running benchmark...")
 
 start_time = time.time()
